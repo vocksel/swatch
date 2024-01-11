c4e540fe8c953bc8f3435ee36fd42da81fc32be1
Marin Minnerly <me@vocksel.com>
2024-01-11 15:46:58 -0800

    Fix log level not persisting

--- server/logging.lua
+++ server/logging.lua
@@ -1,15 +1,9 @@
+local process = require("@lune/process")
 local stdio = require("@lune/stdio")
 
 type LogLevel = "info" | "warning" | "error" | "debug"
 
-export type Logging = {
-	print: typeof(print),
-	logLevel: LogLevel,
-	info: (message: string) -> (),
-	warn: (message: string) -> (),
-	err: (message: string) -> (),
-	debug: (message: string) -> (),
-}
+local LOG_LEVEL: string? = if process.env.LOG_LEVEL then process.env.LOG_LEVEL:lower() else "info"
 
 local LOG_LEVEL_ORDER = {
 	"info",
@@ -18,66 +12,45 @@ local LOG_LEVEL_ORDER = {
 	"error",
 }
 
-local function isLogLevelValid(logLevel: string): boolean
-	return table.find(LOG_LEVEL_ORDER, logLevel) ~= nil
+local function canLog(logLevelToCheck: LogLevel): boolean
+	local maxPriority = table.find(LOG_LEVEL_ORDER, LOG_LEVEL)
+	local priorityToCheck = table.find(LOG_LEVEL_ORDER, logLevelToCheck)
+
+	if maxPriority and priorityToCheck then
+		return priorityToCheck <= maxPriority
+	else
+		return false
+	end
 end
 
-local Logging = {}
+local logging = {}
 
-function Logging.new(): Logging
-	local self = {}
-
-	self.print = print
-	self.stdio = stdio
-	self.logLevel = "info"
-
-	local function shouldLog(logLevel: LogLevel): boolean
-		local maxPriority = table.find(LOG_LEVEL_ORDER, self.logLevel)
-		local priorityToCheck = table.find(LOG_LEVEL_ORDER, logLevel)
-
-		if maxPriority and priorityToCheck then
-			return priorityToCheck <= maxPriority
-		else
-			return false
-		end
+function logging.info(...)
+	if canLog("info") then
+		print(`[info]`, ...)
 	end
-
-	function self.setLogLevel(logLevel: string)
-		logLevel = logLevel:lower()
-		if isLogLevelValid(logLevel) then
-			self.logLevel = logLevel
-		end
-	end
-
-	function self.info(...)
-		if shouldLog("info") then
-			self.print(`[info]`, ...)
-		end
-	end
-
-	function self.warn(...)
-		if shouldLog("warn") then
-			self.stdio.write(stdio.color("yellow"))
-			self.print(`[warn]`, ...)
-			self.stdio.write(stdio.color("reset"))
-		end
-	end
-
-	function self.err(...)
-		if shouldLog("error") then
-			self.stdio.write(stdio.color("red"))
-			self.print(`[err]`, ...)
-			self.stdio.write(stdio.color("reset"))
-		end
-	end
-
-	function self.debug(...)
-		if shouldLog("debug") then
-			self.print(`[debug]`, ...)
-		end
-	end
-
-	return self
 end
 
-return Logging.new()
+function logging.warn(...)
+	if canLog("warning") then
+		stdio.write(stdio.color("yellow"))
+		print(`[warn]`, ...)
+		stdio.write(stdio.color("reset"))
+	end
+end
+
+function logging.err(...)
+	if canLog("error") then
+		stdio.write(stdio.color("red"))
+		print(`[err]`, ...)
+		stdio.write(stdio.color("reset"))
+	end
+end
+
+function logging.debug(...)
+	if canLog("debug") then
+		print(`[debug]`, ...)
+	end
+end
+
+return logging
