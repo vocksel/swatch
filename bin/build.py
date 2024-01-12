a09ae5c5eb5eb5cb1c8a4598f51776298fb26814
Marin Minnerly <me@vocksel.com>
2024-01-12 09:32:53 -0800

    Got lost

--- bin/build.py
+++ bin/build.py
@@ -7,17 +7,22 @@ from pathlib import Path
 parser = ArgumentParser()
 parser.add_argument("--target", choices=["prod", "dev"], required=True)
 parser.add_argument("--output", type=Path, required=True)
+parser.add_argument("--project-path", type=Path)
 parser.add_argument("--watch", action="store_true")
 
 TARGET_TO_PROJECT_FILE_MAP = {
-    "prod": Path("plugin/default.project.json"),
-    "dev": Path("plugin/dev.project.json"),
+    "prod": Path("default.project.json"),
+    "dev": Path("dev.project.json"),
 }
 
 
 def main():
     args = parser.parse_args()
     project_file = TARGET_TO_PROJECT_FILE_MAP[args.target]
+    if args.project_path:
+        project_file = args.project_path / project_file
+
+    print(project_file)
 
     command = ["rojo", "build", project_file, "-o", args.output]
     if args.watch:
