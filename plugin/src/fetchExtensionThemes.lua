c5dc8593245e21acc6a1943a8be30df102851722
Marin Minnerly <me@vocksel.com>
2024-01-16 16:23:44 -0800

    Use hosted API

--- plugin/src/fetchExtensionThemes.lua
+++ plugin/src/fetchExtensionThemes.lua
@@ -3,14 +3,14 @@ local Root = script:FindFirstAncestor("rbxtheme")
 local HttpService = game:GetService("HttpService")
 
 local createUrl = require(Root.createUrl)
+local constants = require(Root.constants)
 local request = require(Root.request)
 local types = require(Root.types)
-local urls = require(Root.urls)
 
 local function fetchExtensionThemes(extension: types.PublishedExtension, version: string)
 	return request({
 		method = "GET",
-		url = createUrl(`{urls.SERVER_URL}/get-themes`, {
+		url = createUrl(`{constants.SERVER_URL}/get-themes`, {
 			extensionName = extension.extensionName,
 			publisherName = extension.publisher.publisherName,
 			extensionVersion = version,
